763ae759f47a4d9f12f31d93b40082872a5c7bb4
ahafez <ah.hafez@gmail.com>
2020-08-14 15:43:54 +0200

    fix gff: resolve ids for all feature not only genes

--- candidamine-bio-sources/candida-gff/src/main/java/org/intermine/bio/dataconversion/CandidaGffGFF3RecordHandler.java
+++ candidamine-bio-sources/candida-gff/src/main/java/org/intermine/bio/dataconversion/CandidaGffGFF3RecordHandler.java
@@ -14,6 +14,9 @@ import org.apache.commons.lang.StringUtils;
 import org.intermine.bio.io.gff3.GFF3Record;
 import org.intermine.metadata.Model;
 import org.intermine.xml.full.Item;
+
+import java.awt.*;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -29,6 +32,7 @@ public class CandidaGffGFF3RecordHandler extends GFF3RecordHandler
      */
     public CandidaGffGFF3RecordHandler (Model model) {
         super(model);
+        // TODO :: revise
         refsAndCollections.put("Exon", "transcripts");
         refsAndCollections.put("MRNA", "gene");
     }
@@ -62,42 +66,91 @@ public class CandidaGffGFF3RecordHandler extends GFF3RecordHandler
         Item feature = getFeature();
         String clsName = feature.getClassName();
 
+        // Bug :: some features are not of type gene
+        // so resolving should be in general regardless of the type
+        // if we have a an id for this feature in our ids then move/swap
+
+
+        // No need for apply this to only genes
+
+
+
+        if (feature.getAttribute("primaryIdentifier") != null) {
+            String secondary = feature.getAttribute("primaryIdentifier").getValue();
+            String primaryIdentifier =  null;
+            String taxonId = getOrganism().getAttribute("taxonId").getValue();
+            if (rslv!= null && rslv.hasTaxon(taxonId)) {
+                int resCount = rslv.countResolutions(taxonId, "gene", secondary);
+                if (resCount == 1) {
+                    primaryIdentifier = rslv.resolveId(taxonId, "gene", secondary).iterator().next();
+                }
+
+            }
+            // if resoved then swap otherwise leave as it is
+            if(primaryIdentifier != null) {
+                feature.setAttribute("secondaryIdentifier", secondary);
+                feature.removeAttribute("primaryIdentifier");
+                if(!StringUtils.isEmpty(primaryIdentifier))
+                    feature.setAttribute("primaryIdentifier", primaryIdentifier);
+            }
+
+
+
+        }
+
+        // if symbol is same as secondaryIdentifier the clean it.
+        if(feature.hasAttribute("secondaryIdentifier")) {
+            String secondary = feature.getAttribute("secondaryIdentifier").getValue();
+            if (feature.hasAttribute("symbol")) {
+                String symbol = feature.getAttribute("symbol").getValue();
+                // System.err.println("Gene symbol " + symbol);
+                // clear duplicate names and symbol if it is the same
+                if (secondary.equals(symbol)) {
+                    feature.removeAttribute("symbol");
+                }
+            }
+        }
         if ("Gene".equals(clsName)) {
             // move Gene.primaryIdentifier to Gene.secondaryIdentifier
             // and remove Gene.primaryIdentifier
 
             //
-            if (feature.getAttribute("primaryIdentifier") != null) {
-                String secondary = feature.getAttribute("primaryIdentifier").getValue();
-                String primaryIdentifier =  null;
-                String taxonId = getOrganism().getAttribute("taxonId").getValue();
-                if (rslv!= null && rslv.hasTaxon(taxonId)) {
-                    int resCount = rslv.countResolutions(taxonId, "gene", secondary);
-                    if (resCount == 1) {
-                        primaryIdentifier = rslv.resolveId(taxonId, "gene", secondary).iterator().next();
-                    }
 
+
+
+        }
+
+        // Problem :: CDS with same ids ??
+        // GFF file from cgd has cds with the same ids :: How to resolve ?
+        // In this configuration CDS correspond to one exon
+        // TOSOLVE :: cache exons by location information
+        // if a CDS has same coordinate then rename the id with the same exon order
+
+        if( "CDS".equals(clsName)) {
+            if ( record.getAttributes().get("Parent") != null )
+            {
+                String transcriptId = record.getAttributes().get("Parent").get(0);
+
+                if(!cdsCountPerTranscript.containsKey(transcriptId)) {
+                    cdsCountPerTranscript.put(transcriptId,0);
                 }
 
+                cdsCountPerTranscript.put(transcriptId,cdsCountPerTranscript.get(transcriptId)+1);
+                int counter = cdsCountPerTranscript.get(transcriptId);
 
-                feature.setAttribute("secondaryIdentifier", secondary);
-                feature.removeAttribute("primaryIdentifier");
-                if(!StringUtils.isEmpty(primaryIdentifier))
-                    feature.setAttribute("primaryIdentifier", primaryIdentifier);
 
+                String primaryIdentifier = feature.getAttribute("primaryIdentifier").getValue();
+                primaryIdentifier = primaryIdentifier + counter;
+                feature.setAttribute("primaryIdentifier", primaryIdentifier);
             }
+            String primaryIdentifier = feature.getAttribute("primaryIdentifier").getValue();
+            System.err.println("Getting CDS " + primaryIdentifier);
 
-            String secondary = feature.getAttribute("secondaryIdentifier").getValue();
-            String symbol = feature.getAttribute("symbol").getValue();
-            // System.err.println("Gene secondary " + secondary);
-            // System.err.println("Gene symbol " + symbol);
-            // clear duplicate names and symbol if it is the same
-            if(secondary.equals(symbol)) {
-                feature.removeAttribute("symbol");
-            }
         }
     }
 
+    // cache features to child
+    HashMap<String,Integer> cdsCountPerTranscript = new HashMap<>();
     @Override
     public void addSynonyms(List<String> synonyms) {
        //this.synonyms.addAll(synonyms);
